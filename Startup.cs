e9852cc791a80460fd22403ad24980e50524b38e
armando kamisaki <akamisaki@gmail.com>
2023-05-26 21:19:36 -0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+
+namespace RazorPagesMovie
+{
+    public class Startup
+    {
+        
+        //private var defaultCulture = new CultureInfo("pt-BR");
+        //private var localizationOptions = new RequestLocalizationOptions
+        //{
+            //DefaultRequestCulture = new RequestCulture(defaultCulture),
+            //SupportedCultures = new List<CultureInfo> { defaultCulture },
+            //SupportedUICultures = new List<CultureInfo> { defaultCulture }
+        //};
+        //app.UseRequestLocalization(localizationOptions);
+
+    }
+    
+}
